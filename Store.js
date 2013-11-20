20362df8a207aed7051a5170b833744a012d15e2
James Brooks <jbrooksuk@me.com>
2013-11-20 20:11:37 +0000

    Storing of posts

--- Store.js
+++ Store.js
@@ -5,12 +5,23 @@ function Store(db) {
 	this.db = new sqlite3.Database(db);
 }
 
-Store.prototype.save = function(data) {
-	var query = this.db.run("INSERT INTO posts (uuid, title, slug, featured, page, status, language, author_id, created_at, created_by) VALUES (?, ?, ?, 0, 0, 'published', 'en_GB', 1, ?, 1)", data.uuid, data.title, data.slug, data.timestamp);
+Store.prototype.save = function(data, callback) {
+	this.db.serialize(function() {
+		if(_.has(data, 'slug') && data.slug !== null) {
+			var query = this.db.prepare("INSERT INTO posts (uuid, title, slug, markdown, featured, page, status, language, author_id, created_at, created_by, updated_at) VALUES (?, ?, ?, ?, 0, ?, ?, 'en_GB', 1, ?, 1, ?)", data.uuid, data.title, data.slug, data.content, data.layout === 'post' ? 0 : 1, data.published ? 'published' : 'draft', data.created_at, data.updated_at);
+
+			query.run();
+			query.finalize();
+		}
+
+		if(typeof callback === 'function') {
+			callback();
+		}
+	}.bind(this));
 };
 
 Store.prototype.close = function() {
 	this.db.close();
-}
+};
 
 module.exports = exports = Store;
